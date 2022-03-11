e2ac74f5b727d03499f30bf991cc746107fa1f4f
raomanoj <manoj9431yadav@gmail.com>
2022-03-12 00:23:57 +0530

    hoisting

---
+++ js/hoisting.js
@@ -0,0 +1,16 @@
+console.log(x);
+greet();
+
+
+var x = 10;
+function greet() {
+    console.log("hello pepcoders");
+}
+//console.log(x);
+//greet();
+
+
+
+
+// JavaScript Hosting refers to the process where by the interpreter appears  to move the declaration or functions, variables or classes to the top of their scope, prior to execution of the code.
+
