c185480457040264db2da69dea72e57135bc9b85
raomanoj <manoj9431yadav@gmail.com>
2022-03-12 01:02:33 +0530

    hoisting in function expression

--- js/hoisting.js
+++ js/hoisting.js
@@ -1,16 +1,23 @@
-console.log(x);
+console.log(a);
 greet();
+console.log(hello);
+hello();
+var a = 10;
 
-
-var x = 10;
+//function
 function greet() {
     console.log("hello pepcoders");
 }
-//console.log(x);
-//greet();
 
+// function expression
+ var hello = function() {
+     console.log("you guys rock");
+ }
 
-
+//hello();
 
 // JavaScript Hosting refers to the process where by the interpreter appears  to move the declaration or functions, variables or classes to the top of their scope, prior to execution of the code.
 
+
+// my definition
+// we can access the varibles and function even before we can declared it. // hosting
