9fb68752031c548574b9d4d9a7a374cb2167902b
raomanoj <manoj9431yadav@gmail.com>
2022-03-12 12:02:40 +0530

    lexical scope, lexical environment, scope chaining

--- js/hoisting.js
+++ js/hoisting.js
@@ -18,6 +18,5 @@ function greet() {
 
 // JavaScript Hosting refers to the process where by the interpreter appears  to move the declaration or functions, variables or classes to the top of their scope, prior to execution of the code.
 
-
 // my definition
 // we can access the varibles and function even before we can declared it. // hosting
